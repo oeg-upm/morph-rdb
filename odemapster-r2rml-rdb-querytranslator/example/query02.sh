cef10226ec84e9de063d31a4e9a463223f5424b4
fpriyatna <freddy.priyatna@gmail.com>
2014-02-25 19:38:41 +0100

    projects cleaning

--- odemapster-r2rml-rdb-querytranslator/example/query02.sh
+++
@@ -1,3 +0,0 @@
-cd ..
-java -cp .:morph.jar:lib/* es.upm.fi.dia.oeg.obdi.wrapper.r2rml.rdb.engine.R2RMLRunner example query02.r2rml.properties
-cd example
--- odemapster-r2rml-rdb-querytranslator/example/query05.sh
+++
@@ -1,3 +0,0 @@
-cd ..
-java -cp .:morph.jar:lib/* es.upm.fi.dia.oeg.obdi.wrapper.r2rml.rdb.engine.R2RMLRunner example query05.r2rml.properties
-cd example
