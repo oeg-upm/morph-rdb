bd536dcbb156768b76caf94fb0c8c28419bcd63b
Freddy Priyatna <freddy.priyatna@gmail.com>
2016-04-14 11:18:27 +0200

    updating examples with csv files

--- morph-examples/src/test/java/es/upm/fi/dia/oeg/morph/example/ExampleWithPropertiesFileCSV.java
+++ morph-examples/src/test/java/es/upm/fi/dia/oeg/morph/example/ExampleWithPropertiesFileCSV.java
@@ -9,7 +9,9 @@ import org.apache.log4j.PropertyConfigurator;
 import org.junit.Test;
 
 import es.upm.fi.dia.oeg.morph.base.engine.MorphBaseRunner;
+
 import es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphRDBRunnerFactory;
+import es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphRDBRunner;
 
 public class ExampleWithPropertiesFileCSV {
     private static Logger logger = Logger.getLogger(ExampleWithPropertiesFileCSV.class);
@@ -19,7 +21,7 @@ public class ExampleWithPropertiesFileCSV {
 
     
     @Test
-    public void test1BatchCSV() {
+    public void testExample1BatchCSV() {
         String configurationFile = "example1-batch-csv.r2rml.properties";
         try {
             MorphRDBRunnerFactory runnerFactory = new MorphRDBRunnerFactory();
@@ -33,4 +35,78 @@ public class ExampleWithPropertiesFileCSV {
         }
     }
 
+    @Test
+    public void testExample1Sparql01CSV() {
+        String configurationFile = "example1-query01-csv.r2rml.properties";
+        try {
+            String[] args = {configurationDirectory, configurationFile};
+            MorphRDBRunner.main(args);
+            System.out.println("Query process DONE------\n\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error : " + e.getMessage());
+            System.out.println("Query process FAILED------\n\n");
+            assertTrue(e.getMessage(), false);
+        }
+    }
+
+    @Test
+    public void testExample1Sparql02CSV() {
+        String configurationFile = "example1-query02-csv.r2rml.properties";
+        try {
+            String[] args = {configurationDirectory, configurationFile};
+            MorphRDBRunner.main(args);
+            System.out.println("Query process DONE------\n\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error : " + e.getMessage());
+            System.out.println("Query process FAILED------\n\n");
+            assertTrue(e.getMessage(), false);
+        }
+    }
+
+    @Test
+    public void testExample1Sparql03CSV() {
+        String configurationFile = "example1-query03-csv.r2rml.properties";
+        try {
+            String[] args = {configurationDirectory, configurationFile};
+            MorphRDBRunner.main(args);
+            System.out.println("Query process DONE------\n\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error : " + e.getMessage());
+            System.out.println("Query process FAILED------\n\n");
+            assertTrue(e.getMessage(), false);
+        }
+    }
+
+    @Test
+    public void testExample1Sparql04CSV() {
+        String configurationFile = "example1-query04-csv.r2rml.properties";
+        try {
+            String[] args = {configurationDirectory, configurationFile};
+            MorphRDBRunner.main(args);
+            System.out.println("Query process DONE------\n\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error : " + e.getMessage());
+            System.out.println("Query process FAILED------\n\n");
+            assertTrue(e.getMessage(), false);
+        }
+    }
+
+    @Test
+    public void testExample1Sparql05CSV() {
+        String configurationFile = "example1-query05-csv.r2rml.properties";
+        try {
+            String[] args = {configurationDirectory, configurationFile};
+            MorphRDBRunner.main(args);
+            System.out.println("Query process DONE------\n\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error : " + e.getMessage());
+            System.out.println("Query process FAILED------\n\n");
+            assertTrue(e.getMessage(), false);
+        }
+    }
 }
