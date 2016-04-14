f30a2e66771eb16f0322c60a91e2548b165284f1
Freddy Priyatna <freddy.priyatna@gmail.com>
2016-04-14 10:22:57 +0200

    updating examples with csv files

---
+++ morph-examples/examples-csv/example1-batch-csv.sh
@@ -0,0 +1,4 @@
+cd ..
+java -cp .:morph-rdb.jar:lib/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphRDBRunner examples-csv example1-batch-csv.r2rml.properties
+cd examples-csv
+
---
+++ morph-examples/examples-csv/example1-query01-csv.sh
@@ -0,0 +1,4 @@
+cd ..
+java -cp .:morph-rdb.jar:lib/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphRDBRunner examples-csv example1-query01-csv.r2rml.properties
+cd examples-csv
+
