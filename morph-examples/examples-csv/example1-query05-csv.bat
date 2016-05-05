e7064e7c2d290cf5acd646b60431c330ee1bc6c6
Freddy Priyatna <freddy.priyatna@gmail.com>
2016-05-05 19:13:04 +0200

    updating windows batch files for examples-csv

--- morph-examples/examples-csv/example1-query05-csv.bat
+++ morph-examples/examples-csv/example1-query05-csv.bat
@@ -1,3 +1,3 @@
 cd ..
-java -cp morph-rdb.jar;lib/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphRDBRunner examples-csv example1-query05-csv.r2rml.properties
+java -cp morph-rdb.jar;lib/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphCSVRunner examples-csv example1-query05-csv.r2rml.properties
 cd examples-csv
