8b891339985ac54f0ad14b25e48209114900b2ab
Freddy Priyatna <fpriyatna@fi.upm.es>
2017-02-24 16:49:09 +0100

    update lib/dependency folder in examples

--- morph-examples/examples-csv/example1-query01-csv.sh
+++ morph-examples/examples-csv/example1-query01-csv.sh
@@ -1,3 +1,3 @@
 cd ..
-java -cp .:morph-rdb.jar:lib/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphCSVRunner examples-csv example1-query01-csv.morph.properties
+java -cp .:morph-rdb-dist-3.8.1.jar:dependency/*  es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphCSVRunner examples-csv example1-query01-csv.morph.properties
 cd examples-csv
