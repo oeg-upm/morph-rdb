b6b57ba08a9092f1081e347340596a47b9b3eebc
Freddy Priyatna <fpriyatna@fi.upm.es>
2017-11-13 12:45:09 +0100

    update historicial buildings csv example

---
+++ morph-examples/examples-csv/edificio-historico-batch-csv.sh
@@ -0,0 +1,3 @@
+cd ..
+java -cp .:morph-rdb-dist-3.8.1.jar:dependency/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphCSVRunner examples-csv edificio-historico-batch-csv.morph.properties
+cd examples-csv
