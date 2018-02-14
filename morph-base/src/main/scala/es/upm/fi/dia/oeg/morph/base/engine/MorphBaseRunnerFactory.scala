18dcc796a4e55bf2a85bc81b1bab529e24806922
fpriyatna <freddy.priyatna@gmail.com>
2018-02-14 12:33:43 +0100

    update log message

--- morph-base/src/main/scala/es/upm/fi/dia/oeg/morph/base/engine/MorphBaseRunnerFactory.scala
+++ morph-base/src/main/scala/es/upm/fi/dia/oeg/morph/base/engine/MorphBaseRunnerFactory.scala
@@ -28,7 +28,7 @@ abstract class MorphBaseRunnerFactory {
 	//val logger = LogManager.getLogger(this.getClass);
 		val logger = LoggerFactory.getLogger(this.getClass());
 
-  logger.info("running morph-rdb 3.9.5 ...");
+  logger.info("running morph-rdb 3.9.6 ...");
 
 	def createRunner(configurationDirectory:String , configurationFile:String)
 	: MorphBaseRunner = {
