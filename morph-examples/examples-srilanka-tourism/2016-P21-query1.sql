c5f038ee834caf1c5a93fbdada5995e92a061b05
Freddy Priyatna <freddy.priyatna@gmail.com>
2018-05-28 12:43:53 +0200

    update example

---
+++ morph-examples/examples-srilanka-tourism/2016-P21-query1.sql
@@ -0,0 +1,58 @@
+SELECT "T2"."COUNTRYOFRESIDENCE" AS "obs",1459016715 AS "mappingid_obs",'http://w3id.org/sri-lanka/tourism/cube-vocab/countryOfResidence' AS "uri_countryOfResidence544834306",'http://w3id.org/sri-lanka/tourism/cube-vocab/month' AS "uri_month884712379",'http://w3id.org/sri-lanka/tourism/cube-vocab/numberOfArrivals' AS "uri_numberOfArrivals1533283167",'http://reference.data.gov.uk/def/intervals/February' AS "var_month",2117099736 AS "mappingid_month","T2"."FEB" AS "noOfArrivals",691863815 AS "mappingid_noOfArrivals"
+FROM "2016-P21" T2
+WHERE T2.COUNTRYOFRESIDENCE = 'Spain'
+
+UNION
+SELECT "T4"."COUNTRYOFRESIDENCE" AS "obs",659996839 AS "mappingid_obs",'http://w3id.org/sri-lanka/tourism/cube-vocab/countryOfResidence' AS "uri_countryOfResidence544834306",'http://w3id.org/sri-lanka/tourism/cube-vocab/month' AS "uri_month884712379",'http://w3id.org/sri-lanka/tourism/cube-vocab/numberOfArrivals' AS "uri_numberOfArrivals1533283167",'http://reference.data.gov.uk/def/intervals/November' AS "var_month",890160158 AS "mappingid_month","T4"."NOV" AS "noOfArrivals",1768127248 AS "mappingid_noOfArrivals"
+FROM "2016-P21" T4
+WHERE T4.COUNTRYOFRESIDENCE = 'Spain'
+
+UNION
+SELECT "T6"."COUNTRYOFRESIDENCE" AS "obs",119112899 AS "mappingid_obs",'http://w3id.org/sri-lanka/tourism/cube-vocab/countryOfResidence' AS "uri_countryOfResidence544834306",'http://w3id.org/sri-lanka/tourism/cube-vocab/month' AS "uri_month884712379",'http://w3id.org/sri-lanka/tourism/cube-vocab/numberOfArrivals' AS "uri_numberOfArrivals1533283167",'http://reference.data.gov.uk/def/intervals/March' AS "var_month",375074687 AS "mappingid_month","T6"."MAR" AS "noOfArrivals",1429616542 AS "mappingid_noOfArrivals"
+FROM "2016-P21" T6
+WHERE T6.COUNTRYOFRESIDENCE = 'Spain'
+
+UNION
+SELECT "T8"."COUNTRYOFRESIDENCE" AS "obs",662666707 AS "mappingid_obs",'http://w3id.org/sri-lanka/tourism/cube-vocab/countryOfResidence' AS "uri_countryOfResidence544834306",'http://w3id.org/sri-lanka/tourism/cube-vocab/month' AS "uri_month884712379",'http://w3id.org/sri-lanka/tourism/cube-vocab/numberOfArrivals' AS "uri_numberOfArrivals1533283167",'http://reference.data.gov.uk/def/intervals/August' AS "var_month",1821100643 AS "mappingid_month","T8"."AUG" AS "noOfArrivals",593768538 AS "mappingid_noOfArrivals"
+FROM "2016-P21" T8
+WHERE T8.COUNTRYOFRESIDENCE = 'Spain'
+
+UNION
+SELECT "T10"."COUNTRYOFRESIDENCE" AS "obs",1191864687 AS "mappingid_obs",'http://w3id.org/sri-lanka/tourism/cube-vocab/countryOfResidence' AS "uri_countryOfResidence544834306",'http://w3id.org/sri-lanka/tourism/cube-vocab/month' AS "uri_month884712379",'http://w3id.org/sri-lanka/tourism/cube-vocab/numberOfArrivals' AS "uri_numberOfArrivals1533283167",'http://reference.data.gov.uk/def/intervals/September' AS "var_month",466032056 AS "mappingid_month","T10"."SEP" AS "noOfArrivals",690822257 AS "mappingid_noOfArrivals"
+FROM "2016-P21" T10
+WHERE T10.COUNTRYOFRESIDENCE = 'Spain'
+
+UNION
+SELECT "T12"."COUNTRYOFRESIDENCE" AS "obs",726212590 AS "mappingid_obs",'http://w3id.org/sri-lanka/tourism/cube-vocab/countryOfResidence' AS "uri_countryOfResidence544834306",'http://w3id.org/sri-lanka/tourism/cube-vocab/month' AS "uri_month884712379",'http://w3id.org/sri-lanka/tourism/cube-vocab/numberOfArrivals' AS "uri_numberOfArrivals1533283167",'http://reference.data.gov.uk/def/intervals/January' AS "var_month",1058822905 AS "mappingid_month","T12"."JAN" AS "noOfArrivals",475394679 AS "mappingid_noOfArrivals"
+FROM "2016-P21" T12
+WHERE T12.COUNTRYOFRESIDENCE = 'Spain'
+
+UNION
+SELECT "T14"."COUNTRYOFRESIDENCE" AS "obs",1574615832 AS "mappingid_obs",'http://w3id.org/sri-lanka/tourism/cube-vocab/countryOfResidence' AS "uri_countryOfResidence544834306",'http://w3id.org/sri-lanka/tourism/cube-vocab/month' AS "uri_month884712379",'http://w3id.org/sri-lanka/tourism/cube-vocab/numberOfArrivals' AS "uri_numberOfArrivals1533283167",'http://reference.data.gov.uk/def/intervals/October' AS "var_month",1049590050 AS "mappingid_month","T14"."OCT" AS "noOfArrivals",1662592920 AS "mappingid_noOfArrivals"
+FROM "2016-P21" T14
+WHERE T14.COUNTRYOFRESIDENCE = 'Spain'
+
+UNION
+SELECT "T16"."COUNTRYOFRESIDENCE" AS "obs",1357686726 AS "mappingid_obs",'http://w3id.org/sri-lanka/tourism/cube-vocab/countryOfResidence' AS "uri_countryOfResidence544834306",'http://w3id.org/sri-lanka/tourism/cube-vocab/month' AS "uri_month884712379",'http://w3id.org/sri-lanka/tourism/cube-vocab/numberOfArrivals' AS "uri_numberOfArrivals1533283167",'http://reference.data.gov.uk/def/intervals/June' AS "var_month",1491522744 AS "mappingid_month","T16"."JUN" AS "noOfArrivals",501608687 AS "mappingid_noOfArrivals"
+FROM "2016-P21" T16
+WHERE T16.COUNTRYOFRESIDENCE = 'Spain'
+
+UNION
+SELECT "T18"."COUNTRYOFRESIDENCE" AS "obs",929706284 AS "mappingid_obs",'http://w3id.org/sri-lanka/tourism/cube-vocab/countryOfResidence' AS "uri_countryOfResidence544834306",'http://w3id.org/sri-lanka/tourism/cube-vocab/month' AS "uri_month884712379",'http://w3id.org/sri-lanka/tourism/cube-vocab/numberOfArrivals' AS "uri_numberOfArrivals1533283167",'http://reference.data.gov.uk/def/intervals/December' AS "var_month",42820240 AS "mappingid_month","T18"."DEC" AS "noOfArrivals",1822434669 AS "mappingid_noOfArrivals"
+FROM "2016-P21" T18
+WHERE T18.COUNTRYOFRESIDENCE = 'Spain'
+
+UNION
+SELECT "T20"."COUNTRYOFRESIDENCE" AS "obs",1592840862 AS "mappingid_obs",'http://w3id.org/sri-lanka/tourism/cube-vocab/countryOfResidence' AS "uri_countryOfResidence544834306",'http://w3id.org/sri-lanka/tourism/cube-vocab/month' AS "uri_month884712379",'http://w3id.org/sri-lanka/tourism/cube-vocab/numberOfArrivals' AS "uri_numberOfArrivals1533283167",'http://reference.data.gov.uk/def/intervals/July' AS "var_month",955611965 AS "mappingid_month","T20"."JUL" AS "noOfArrivals",1367672657 AS "mappingid_noOfArrivals"
+FROM "2016-P21" T20
+WHERE T20.COUNTRYOFRESIDENCE = 'Spain'
+
+UNION
+SELECT "T22"."COUNTRYOFRESIDENCE" AS "obs",1021565761 AS "mappingid_obs",'http://w3id.org/sri-lanka/tourism/cube-vocab/countryOfResidence' AS "uri_countryOfResidence544834306",'http://w3id.org/sri-lanka/tourism/cube-vocab/month' AS "uri_month884712379",'http://w3id.org/sri-lanka/tourism/cube-vocab/numberOfArrivals' AS "uri_numberOfArrivals1533283167",'http://reference.data.gov.uk/def/intervals/April' AS "var_month",1692375649 AS "mappingid_month","T22"."APR" AS "noOfArrivals",2002790740 AS "mappingid_noOfArrivals"
+FROM "2016-P21" T22
+WHERE T22.COUNTRYOFRESIDENCE = 'Spain'
+
+UNION
+SELECT "T24"."COUNTRYOFRESIDENCE" AS "obs",237373286 AS "mappingid_obs",'http://w3id.org/sri-lanka/tourism/cube-vocab/countryOfResidence' AS "uri_countryOfResidence544834306",'http://w3id.org/sri-lanka/tourism/cube-vocab/month' AS "uri_month884712379",'http://w3id.org/sri-lanka/tourism/cube-vocab/numberOfArrivals' AS "uri_numberOfArrivals1533283167",'http://reference.data.gov.uk/def/intervals/May' AS "var_month",742255664 AS "mappingid_month","T24"."MAY" AS "noOfArrivals",883521348 AS "mappingid_noOfArrivals"
+FROM "2016-P21" T24
+WHERE T24.COUNTRYOFRESIDENCE = 'Spain'
