5d7ef60811607376b92db41de13f876c85119f9f
Freddy Priyatna <freddy.priyatna@gmail.com>
2018-06-13 16:08:12 +0200

    add queries for example eurostat immigration

--- morph-examples/src/test/java/es/upm/fi/dia/oeg/morph/example/ExampleEuroStatImmigration.java
+++ morph-examples/src/test/java/es/upm/fi/dia/oeg/morph/example/ExampleEuroStatImmigration.java
@@ -41,6 +41,55 @@ public class ExampleEuroStatImmigration {
         }
     }
 
+
+    @Test
+    public void testQuery01() {
+        String configurationDirectory = System.getProperty("user.dir") + "/examples-eurostat";
+        String configurationFile = "eurostatimmigration-query1.morph.properties";
+        try {
+            MorphCSVRunnerFactory runnerFactory = new MorphCSVRunnerFactory();
+            MorphBaseRunner runner = runnerFactory.createRunner(configurationDirectory, configurationFile);
+            runner.run();
+            System.out.println("Batch process DONE------\n\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Batch process FAILED------\n\n");
+            assertTrue(e.getMessage(), false);
+        }
+    }
+
+    @Test
+    public void testQuery02() {
+        String configurationDirectory = System.getProperty("user.dir") + "/examples-eurostat";
+        String configurationFile = "eurostatimmigration-query2.morph.properties";
+        try {
+            MorphCSVRunnerFactory runnerFactory = new MorphCSVRunnerFactory();
+            MorphBaseRunner runner = runnerFactory.createRunner(configurationDirectory, configurationFile);
+            runner.run();
+            System.out.println("Batch process DONE------\n\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Batch process FAILED------\n\n");
+            assertTrue(e.getMessage(), false);
+        }
+    }
+
+    @Test
+    public void testQuery03() {
+        String configurationDirectory = System.getProperty("user.dir") + "/examples-eurostat";
+        String configurationFile = "eurostatimmigration-query3.morph.properties";
+        try {
+            MorphCSVRunnerFactory runnerFactory = new MorphCSVRunnerFactory();
+            MorphBaseRunner runner = runnerFactory.createRunner(configurationDirectory, configurationFile);
+            runner.run();
+            System.out.println("Batch process DONE------\n\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Batch process FAILED------\n\n");
+            assertTrue(e.getMessage(), false);
+        }
+    }
+
     @Test
     public void testRangeBatch() {
         String configurationDirectory = System.getProperty("user.dir") + "/examples-eurostat";
