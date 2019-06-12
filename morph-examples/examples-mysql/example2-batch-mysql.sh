14c166c7889ea4caed0e2a6a1714b7c6d0266bad
Freddy Priyatna <freddy.priyatna@gmail.com>
2019-06-12 15:30:56 +0200

    add example 2

---
+++ morph-examples/examples-mysql/example2-batch-mysql.sh
@@ -0,0 +1,4 @@
+cd ..
+java -cp .:morph-rdb-dist-3.9.17.jar:lib/*:dependency/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphRDBRunner examples-mysql example2-batch-mysql.morph.properties
+cd examples-mysql
+
