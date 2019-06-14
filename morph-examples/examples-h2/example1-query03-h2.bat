168e97e1f8b9c7ff0b9007e4eb1e1f4be8e44ed3
Freddy Priyatna <freddy.priyatna@gmail.com>
2019-06-14 14:54:17 +0200

    reorganize h2 examples batch files

--- morph-examples/examples-h2/example1-query03-h2.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-java -cp morph-rdb-dist-3.9.17.jar;dependency/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphRDBRunner examples-h2 example1-query03-h2.r2rml.properties
-cd examples-h2
---
+++ morph-examples/examples-h2/h2-example1-query02.bat
@@ -0,0 +1,3 @@
+cd ..
+java -cp morph-rdb.jar;dependency/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphRDBRunner examples-h2 example1-query02-h2.r2rml.properties
+cd examples-h2
