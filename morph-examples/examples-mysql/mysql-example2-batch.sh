159c0f9bbe62d846ebbe85084184a46239c7be89
Freddy Priyatna <freddy.priyatna@gmail.com>
2019-06-18 18:20:32 +0200

    remove unused files

--- morph-examples/examples-mysql/mysql-example2-batch.sh
+++
@@ -1,4 +0,0 @@
-cd ..
-java -cp .:morph-rdb.jar:lib/*:dependency/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphRDBRunner examples-mysql example2-batch-mysql.morph.properties
-cd examples-mysql
-
--- morph-examples/examples-mysql/mysql-example2-query07.sh
+++
@@ -1,3 +0,0 @@
-cd ..
-java -cp .:morph-rdb.jar:lib/*:dependency/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphRDBRunner examples-mysql example2-query07-mysql.morph.properties
-cd examples-mysql
