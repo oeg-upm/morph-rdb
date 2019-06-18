1b7d73f0f9526ab60ab75ce6fc86d95d4a6e787d
Freddy Priyatna <freddy.priyatna@gmail.com>
2019-06-18 18:05:44 +0200

    add compose.sh

---
+++ morph-examples/examples-mysql/compose.sh
@@ -0,0 +1,11 @@
+docker stop morph-example-mysql
+docker rm morph-example-mysql
+docker rmi morph-example-mysql
+docker stop drupal
+docker rm drupal
+docker rmi drupal
+docker stop mysql-morph
+docker rm mysql-morph
+docker rmi mysql-morph
+docker-compose up -d
+
