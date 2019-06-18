dc0089ed4ab9350d15865e6429512c771a4a2b07
Jhon Toledo <jhontb7@gmail.com>
2019-06-18 09:16:19 +0000

    add batch file docker start

--- morph-examples/examples-mysql/sql-scripts/run.sh
+++ morph-examples/examples-mysql/sql-scripts/run.sh
@@ -2,3 +2,5 @@ docker stop mysql-morph
 docker rm mysql-morph
 docker build -t mysql-morph .
 docker run -d -p 3307:3306 --name mysql-morph -e MYSQL_ROOT_PASSWORD=password mysql-morph
+docker start mysql-morph
+docker ps -a
