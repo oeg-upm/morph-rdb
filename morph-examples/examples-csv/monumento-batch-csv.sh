021138bf987906c06dd24ac70f7182fc6eabeb3e
Freddy Priyatna <freddy.priyatna@gmail.com>
2020-09-02 09:26:22 -0400

    update csv examples

--- morph-examples/examples-csv/monumento-batch-csv.sh
+++ morph-examples/examples-csv/monumento-batch-csv.sh
@@ -1,3 +1,3 @@
 cd ..
-java -cp .:morph-rdb-dist-3.8.1.jar:dependency/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphCSVRunner examples-csv monumento-batch-csv.morph.properties
+java -cp .:morph-rdb.jar:dependency/* es.upm.fi.dia.oeg.morph.r2rml.rdb.engine.MorphCSVRunner examples-csv monumento-batch-csv.morph.properties
 cd examples-csv
